617e65c94019d67f4a352e1756fea54dfe89e5b1
june lee <melodysdreamj@gmail.com>
2024-03-23 22:35:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+[![pub package](https://img.shields.io/pub/v/june_view.svg)](https://pub.dartlang.org/packages/june_view)
+
+# june_view
+this module is a part of the juneflow project, it provides a view module for flutter project.
+
+##  Installation
+1. If the juneflow project doesn't exist, please create it by following [this guide](https://doc.juneflow.org/get-started).
+2. open terminal in the juneflow project root directory, enter the following command.
+ ```bash
+ june add june_view
+ ```
+
+## Usage
+you can find the usage of view in the [official document](https://pub.dev/packages/june_view).
---
+++ lib/util/config/_/global_imports.dart
@@ -0,0 +1,21 @@
+export '../../_/shared_params/_/start_app_params.dart';
+
+export 'package:flutter/services.dart';
+
+export '../../_/initial_app/build_app_widget/build_run_app/_.dart';
+
+export 'package:june_flow_util/june_flow_util.dart';
+
+export 'package:june/june.dart'; //@add
+export 'package:styled_widget/styled_widget.dart'; //@add
+export '../../_/shared_params/hive_module/_.dart';
+
+export '../../_/shared_params/hive_module/_.dart';
+
+export '../../_/shared_params/hive_module/_.dart';
+
+export '../../_/shared_params/hive_module/_.dart';
+
+export '../../_/shared_params/hive_module/_.dart';
+
+export 'package:flutter_screenutil/flutter_screenutil.dart';
